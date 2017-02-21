7c2b2a01339e3860e584d77dbd1eb63003e2368b
Waffesrgud <gcor8853@740s-pl-132.local>
2017-02-21 08:38:17 -0700

    friccin STOP GAH THIS IS SO IRRITATING C++ IS THE WORST LANGUAGE

--- Array.hpp
+++ Array.hpp
@@ -10,7 +10,10 @@
 #define Array_hpp
 
 #include <assert.h>
+#include <iostream>
 #include "Node.hpp"
+
+using namespace std;
 
 template <class Type>
 class Array
@@ -19,11 +22,38 @@ private:
     Node<Type> * front;
     int size;
 public:
+    Array<Type>();
     Array<Type>(int size);
-    int getSize();
+    
+    ~Array<Type>();
+    Array<Type>(const Array<Type> & toBeCopied);
+    void operator = (const Array<Type> & toBeAssigned);
+    
     Type getFromIndex(int index);
     void setAtIndex(int index, Type value);
+    
+    int getSize() const;
+    Node<Type> * getFront() const;
 };
+
+template <class Type>
+Array<Type> :: ~Array()
+{
+    int count = size;
+    Node<Type> * remove = front;
+    while(front != nullptr)
+    {
+
+        front = front->getNullPointer();
+        cout << "Moving to the next node. At: "  << count << endl;
+        delete remove;
+        cout << "Front is deleting..." << endl;
+        remove = front;
+        cout << "Moving front..." << endl;
+        count--;
+        cout << "Front is at: " << front << " count is: " << count << endl;
+    }
+}
 
 template <class Type>
 Array<Type> :: Array(int size)
@@ -31,14 +61,35 @@ Array<Type> :: Array(int size)
     assert(size > 0);
     
     this->size = size;
-    
     for(int index = 1; index < size; index++)
     {
-        IntNode * currentNode = new IntNode();
+        Node<Type> * currentNode = new IntNode();
         currentNode->setNodePointer(front);
         front = currentNode;
     }
     
+}
+
+template <class Type>
+Array<Type> :: Array(const Array<Type> & toBeCopied)
+{
+    this-> size = toBeCopied.getSize();
+    this-> front = new Node<Type>();
+    for(int index = 1; index < size; index++)
+    {
+        Node<Type> * temp = new Node<Type>();
+        temp->setNodePointer(front);
+        front = temp;
+    }
+    
+    Node<Type> * copyTemp = toBeCopied.getFront();
+    Node<Type> * updated = this->front;
+    for(int index = 0; index < size; index++)
+    {
+        updated->setNodeData(copyTemp-> getNodeData());
+        updated = updated->getNodePointer();
+        copyTemp = copyTemp->getNodePointer();
+    }
 }
 
 template <class Type>
@@ -48,14 +99,14 @@ Array<Type> :: Array()
 }
 
 template <class Type>
-int Array<Type> :: getIndex(int index)
+Type Array<Type> :: getFromIndex(int index)
 {
     assert(index >= 0 && index < size);
     int value = 0;
-    
+    Node<Type> * curr = front;
     for(int position = 0; position < index; position++)
     {
-        current = current->getNodePointer();
+        curr = curr->getNodePointer();
     }
     
     value = current->getNodeData();
@@ -63,7 +114,19 @@ int Array<Type> :: getIndex(int index)
 }
 
 template <class Type>
-void Array<Type> :: setAtIndex(int index, int value)
+Node<Type> * Array<Type> :: getFront() const
+{
+    return front;
+}
+
+template <class Type>
+int Array<Type> :: getSize() const
+{
+    return size;
+}
+
+template <class Type>
+void Array<Type> :: setAtIndex(int index, Type value)
 {
     assert(index >= 0 && index < size);
     
